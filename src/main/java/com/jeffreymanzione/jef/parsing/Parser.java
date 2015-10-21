1cc576cdcf1112ac1e34a31ca8cb76e514594d3e
Jeff <Jeff@129.2.205.147>
2015-10-20 22:27:32 -0400

    Updated javadoc for Parser

--- src/main/java/com/jeffreymanzione/jef/parsing/Parser.java
+++ src/main/java/com/jeffreymanzione/jef/parsing/Parser.java
@@ -6,15 +6,46 @@ import com.jeffreymanzione.jef.parsing.exceptions.IndexableException;
 import com.jeffreymanzione.jef.parsing.value.MapValue;
 import com.jeffreymanzione.jef.tokenizing.Token;
 
+/**
+ * 
+ * @author Jeffrey J. Manzione
+ * 
+ *         A Parser takes in {@link Queue}<{@link Token}> and outputs a {@link MapValue} which represents those tokens.
+ *         The starting point for a Parser is {@link #parse(Queue)}.
+ *
+ */
 public interface Parser {
+	/**
+	 * If parse has been called, hasErrors() indicates whether or not there were errors in parsing. If
+	 * {@link #parse(Queue)} has not been run, it returns false.
+	 * 
+	 * @return true if there were errors during parsing, false otherwise.
+	 */
+	public boolean hasErrors ();
 
-  public boolean hasErrors();
+	/**
+	 * 
+	 * @return A {@link ValidateResponse} which includes all validation information after {@link #parse(Queue)} has been
+	 *         run.
+	 */
+	public ValidationResponse getExceptions ();
 
-  public ValidationResponse getExceptions();
+	/**
+	 * Sets the verbosity of the parser. The implementation determines exactly how verbose "verbose" is.
+	 * @param isVerbose
+	 */
+	public void setVerbose ( boolean isVerbose );
+	/**
+	 * 
+	 * @return	true if the parse has been set to be verbose, false otherwise.
+	 */
+	public boolean isVerbose ();
 
-  public void setVerbose(boolean isVerbose);
-
-  public boolean isVerbose();
-
-  public MapValue parse(Queue<Token> tokens) throws IndexableException;
+	/**
+	 * Parses a {@link Queue}<{@link Token}> and outputs a {@link MapValue} which represents the parsed tokens.
+	 * @param tokens
+	 * @return
+	 * @throws IndexableException
+	 */
+	public MapValue parse ( Queue<Token> tokens ) throws IndexableException;
 }
